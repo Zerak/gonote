af9a1162cf7f2ba46bc13c49202484fa7ef33096
zerak <Kingbug2010@126.com>
2016-09-02 19:01:37 +0800

    operator & <<

--- operator/main.go
+++ operator/main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Printf("&operator %v\n", 511 & 1)
-	fmt.Printf("&operator %v\n", 511 & 2)
-	fmt.Printf("&operator %v\n", 511 & 4)
-	fmt.Printf("&operator %v\n", 511 & 8)
-	fmt.Printf("&operator %v\n", 511 & 16)
-	fmt.Printf("&operator %v\n", 511 & 32)
-	fmt.Printf("&operator %v\n", 511 & 64)
-	fmt.Printf("&operator %v\n", 511 & 128)
-	fmt.Printf("&operator %v\n", 511 & 512)
+	fmt.Printf("&operator %v\n", 447 & 1)
+	fmt.Printf("&operator %v\n", 447 & 2)
+	fmt.Printf("&operator %v\n", 447 & 4)
+	fmt.Printf("&operator %v\n", 447 & 8)
+	fmt.Printf("&operator %v\n", 447 & 16)
+	fmt.Printf("&operator %v\n", 447 & 32)
+	fmt.Printf("&operator %v\n", 447 & 64)
+	fmt.Printf("&operator %v\n", 447 & 128)
+	fmt.Printf("&operator %v\n", 447 & 256)
+	fmt.Printf("&operator %v\n", 447 & 512)
 	fmt.Printf("&operator %v\n", 1023 & 1024)
 }
