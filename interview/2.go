5c8348d8c38c8eaedd1313f13a0ebbde763d99ae
zerak <Kingbug2010@126.com>
2017-11-10 22:38:37 +0800

    slice op

--- interview/2.go
+++ interview/2.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := [5]int{1, 2, 3, 4, 5}
+	s1 := arr[1:4]
+	fmt.Println("s1 len:", len(s1), " cap:", cap(s1))
+	fmt.Println(s1)
+
+	s2 := s1[2:]
+	fmt.Println("s2 len:", len(s2), " cap:", cap(s2))
+	fmt.Println(s2)
 }
