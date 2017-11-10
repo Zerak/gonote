b7723905fd1ff4b5d7936feb9b18132b84807501
zerak <Kingbug2010@126.com>
2017-11-10 22:44:36 +0800

    code format

--- interview/3.go
+++ interview/3.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func fix(s []int) {
-	tmp := make([]int,3)
-	s = append(s,tmp...)
+	tmp := make([]int, 3)
+	s = append(s, tmp...)
 }
 
 func main() {
-	s := []int{1,2,3}
+	s := []int{1, 2, 3}
 	fix(s)
 	fmt.Println(s)
 }
